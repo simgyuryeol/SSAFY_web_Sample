5218091eb27bdf636813703f21cb4004fce9ee8c
simgyuryeol <66519915+simgyuryeol@users.noreply.github.com>
2023-03-25 19:38:08 +0900

    FEAT : 상품목록 기능 구현

--- web_Sample/src/com/ssafy/sample/model/dao/ProductDaoImpl.java
+++ web_Sample/src/com/ssafy/sample/model/dao/ProductDaoImpl.java
@@ -2,6 +2,8 @@ package com.ssafy.sample.model.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.ssafy.sample.dto.productDto;
@@ -51,8 +53,44 @@ public class ProductDaoImpl implements ProductDao {
 
 	@Override
 	public List<productDto> ProductList(int limit, int offset) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		List<productDto> list = new ArrayList<>();
+		
+		
+		try {
+			con = DBUtil.getInstance().getConnection();
+			//select 조회
+			StringBuilder sb = new StringBuilder();
+			sb.append("SELECT code, model, price ")
+			.append(" FROM product ")
+			.append(" ORDER BY code ")
+			.append(" LIMIT ? OFFSET ? ");
+			
+			pstmt = con.prepareStatement(sb.toString());
+			pstmt.setInt(1, limit);
+			pstmt.setInt(2, offset);
+			
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				productDto productdto = new productDto();
+				productdto.setCode(rs.getString("code"));
+				productdto.setModel(rs.getString("model"));
+				productdto.setPrice(rs.getInt("price"));
+				list.add(productdto);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBUtil.getInstance().close(con,pstmt,rs);
+		}
+		
+		return list;
+		
 	}
 	
 	
